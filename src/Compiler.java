ea909037b442f1b5a3a44287d5957523ca74eb40
Jerx2y <jerx2y@outlook.com>
2023-02-04 12:29:17 +0800

    [C] Add minimal evaluation

--- src/Compiler.java
+++ src/Compiler.java
@@ -34,12 +34,12 @@ public class Compiler {
 
         if (!online) {
             input = new FileInputStream("testcase/t54.mx");
-            IROutput = new PrintStream(new FileOutputStream("testcase/test.ll"));
+            // IROutput = new PrintStream(new FileOutputStream("testcase/test.ll"));
             BuiltinOutput = new PrintStream(new FileOutputStream("testcase/builtin.s"));
             AsmOutput = new PrintStream(new FileOutputStream("testcase/test.s"));
         } else {
             input = System.in;
-            IROutput = new PrintStream(new FileOutputStream("test.ll"));
+            // IROutput = new PrintStream(new FileOutputStream("test.ll"));
             BuiltinOutput = new PrintStream(new FileOutputStream("builtin.s"));
             AsmOutput = new PrintStream(new FileOutputStream("output.s"));
         }
@@ -65,7 +65,7 @@ public class Compiler {
             IRModule irModule = new IRModule();
             new IRCollector(gScope, irModule).visit(ASTRoot);
             new IRBuilder(gScope, irModule).visit(ASTRoot);
-            if (!online) new IRPrinter(IROutput).visit(irModule);
+            // if (!online) new IRPrinter(IROutput).visit(irModule);
 
             AsmModule asmModule = new AsmModule();
             new InsSelector(asmModule).visit(irModule);
